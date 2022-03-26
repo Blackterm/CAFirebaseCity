334d42e3a976d91c607015f9e0b677e848ada99b
Murat Can <59890493+Blackterm@users.noreply.github.com>
2022-03-26 18:31:01 +0300

    fix something

--- lib/App/pages/addCity/addCity_view.dart
+++ lib/App/pages/addCity/addCity_view.dart
@@ -1,4 +1,5 @@
 import 'package:ca_firebase_app/App/pages/addCity/addCity_controller.dart';
+import 'package:ca_firebase_app/App/pages/home/home_view.dart';
 import 'package:ca_firebase_app/Data/repositories/data_city_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
@@ -24,137 +25,183 @@ class _AddCityViewState extends ViewState<AddCityView, AddCityController> {
       key: globalKey,
       body: ControlledWidgetBuilder<AddCityController>(
         builder: (context, controller) {
-          return SingleChildScrollView(
+          return SafeArea(
             child: Column(
               children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(
-                        top: 52,
-                        left: 24,
-                        right: 18,
-                      ),
-                      child: Image(
-                          image: NetworkImage(
-                              "https://s3-alpha-sig.figma.com/img/4bbe/6cfc/8b54021292fe48bf1fd630972d01258e?Expires=1648425600&Signature=EIb61Bh4pkbCGKkZ8BiJ~41wWfJWAG-YfnauvtUTRMBNTMS1r5dPmVkL9QajP1YvvhQvSb97h4p0nFlsqnbevbv4seX2B6AbiHW2GWIaThIw21KXsHErpaOMvRd2KshrRDLHiL5NPF1tL6k20ZDYP7H-FdkooOFUNRKMtW6CJgLL0VLYwL13iYwY0MVzlRTEoN0dOgRBhiTq7P1xSBMMAeEnJEZwOW8Is4W-hn1CurfNq2V7QmCKxcYYNB7M0c4soGTRB0kf8RJ-p4XIFw3tQjJsmfPnqMBLSQYER-hb78feiITcTXSmO~HF~SM3ecaM6pxnkNsg-Y3psV~uKBkpcQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")),
-                    ),
-                    SizedBox(
-                      height: 35,
-                    ),
-                    Container(
-                      width: size.width - 36,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromARGB(235, 221, 218, 218),
-                            offset: Offset(5, 2),
-                            blurRadius: 4.0,
-                            spreadRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                      child: TextFormField(
-                        keyboardType: TextInputType.text,
-                        onChanged: (value) =>
-                            {controller.onCityNameChanged(value)},
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "  " + 'Şehirin Adı',
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      width: size.width - 36,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromARGB(235, 221, 218, 218),
-                            offset: Offset(5, 2),
-                            blurRadius: 4.0,
-                            spreadRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                      child: TextFormField(
-                        //obscureText: true,
-                        keyboardType: TextInputType.text,
-                        onChanged: (value) =>
-                            {controller.onContentChanged(value)},
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "  " + 'Şehirin Açıklaması',
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      width: size.width - 36,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromARGB(235, 221, 218, 218),
-                            offset: Offset(5, 2),
-                            blurRadius: 4.0,
-                            spreadRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                      child: TextFormField(
-                        keyboardType: TextInputType.text,
-                        onChanged: (value) =>
-                            {controller.onImageUrlChanged(value)},
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "  " + 'Şehirin resmi',
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    GestureDetector(
+                Container(
+                  width: size.width,
+                  height: 56,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF6c63ff),
+                  ),
+                  child: Row(
+                    children: [
+                      GestureDetector(
                         onTap: () {
-                          controller.areFieldsIsEmpty
-                              ? controller.addCity()
-                              : null;
+                          Navigator.push(
+                            (context),
+                            MaterialPageRoute(
+                              builder: (context) => HomeView(),
+                            ),
+                          );
+                          ;
                         },
-                        child: Container(
-                          width: 127,
-                          height: 53,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF6c63ff),
-                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Icon(
+                            Icons.home,
+                            color: Colors.white,
                           ),
-                          child: Center(
-                            child: Text(
-                              "Şehiri Ekle",
-                              style: TextStyle(
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 120),
+                        child: Text(
+                          "Şehir Ekle",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(
+                                top: 52,
+                                left: 24,
+                                right: 18,
+                              ),
+                              child: Image(
+                                  image: NetworkImage(
+                                      "https://s3-alpha-sig.figma.com/img/4bbe/6cfc/8b54021292fe48bf1fd630972d01258e?Expires=1648425600&Signature=EIb61Bh4pkbCGKkZ8BiJ~41wWfJWAG-YfnauvtUTRMBNTMS1r5dPmVkL9QajP1YvvhQvSb97h4p0nFlsqnbevbv4seX2B6AbiHW2GWIaThIw21KXsHErpaOMvRd2KshrRDLHiL5NPF1tL6k20ZDYP7H-FdkooOFUNRKMtW6CJgLL0VLYwL13iYwY0MVzlRTEoN0dOgRBhiTq7P1xSBMMAeEnJEZwOW8Is4W-hn1CurfNq2V7QmCKxcYYNB7M0c4soGTRB0kf8RJ-p4XIFw3tQjJsmfPnqMBLSQYER-hb78feiITcTXSmO~HF~SM3ecaM6pxnkNsg-Y3psV~uKBkpcQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")),
+                            ),
+                            SizedBox(
+                              height: 35,
+                            ),
+                            Container(
+                              width: size.width - 36,
+                              height: 56,
+                              decoration: BoxDecoration(
                                 color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 18,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color.fromARGB(235, 221, 218, 218),
+                                    offset: Offset(5, 2),
+                                    blurRadius: 4.0,
+                                    spreadRadius: 1.0,
+                                  ),
+                                ],
+                              ),
+                              child: TextFormField(
+                                keyboardType: TextInputType.text,
+                                onChanged: (value) =>
+                                    {controller.onCityNameChanged(value)},
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "  " + 'Şehirin Adı',
+                                ),
                               ),
                             ),
-                          ),
-                        )),
-                    SizedBox(
-                      height: 20,
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            Container(
+                              width: size.width - 36,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color.fromARGB(235, 221, 218, 218),
+                                    offset: Offset(5, 2),
+                                    blurRadius: 4.0,
+                                    spreadRadius: 1.0,
+                                  ),
+                                ],
+                              ),
+                              child: TextFormField(
+                                //obscureText: true,
+                                keyboardType: TextInputType.text,
+                                onChanged: (value) =>
+                                    {controller.onContentChanged(value)},
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "  " + 'Şehirin Açıklaması',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            Container(
+                              width: size.width - 36,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color.fromARGB(235, 221, 218, 218),
+                                    offset: Offset(5, 2),
+                                    blurRadius: 4.0,
+                                    spreadRadius: 1.0,
+                                  ),
+                                ],
+                              ),
+                              child: TextFormField(
+                                keyboardType: TextInputType.text,
+                                onChanged: (value) =>
+                                    {controller.onImageUrlChanged(value)},
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "  " + 'Şehirin resmi',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 50,
+                            ),
+                            GestureDetector(
+                                onTap: () {
+                                  controller.areFieldsIsEmpty
+                                      ? controller.addCity()
+                                      : null;
+                                },
+                                child: Container(
+                                  width: 127,
+                                  height: 53,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFF6c63ff),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8)),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Şehiri Ekle",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ),
+                                )),
+                            SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
--- lib/App/pages/cityDetail/cityDetail_view.dart
+++ lib/App/pages/cityDetail/cityDetail_view.dart
@@ -34,110 +34,117 @@ class _CityDetailViewState
           return SafeArea(
             child: Scaffold(
               backgroundColor: Colors.white,
-              body: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      width: size.width,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF6c63ff),
-                      ),
-                      child: Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                (context),
-                                MaterialPageRoute(
-                                  builder: (context) => HomeView(),
-                                ),
-                              );
-                              ;
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 20),
-                              child: Icon(
-                                Icons.home,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 140),
-                            child: Text(
-                              widget._city.cityName,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+              body: Column(
+                children: [
+                  Container(
+                    width: size.width,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF6c63ff),
                     ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 20, top: 45, right: 20),
-                      child: Row(
-                        children: [
-                          Text(
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              (context),
+                              MaterialPageRoute(
+                                builder: (context) => HomeView(),
+                              ),
+                            );
+                            ;
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: Icon(
+                              Icons.home,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 140),
+                          child: Text(
                             widget._city.cityName,
                             style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w700,
-                                color: Color(0xFF6c63ff)),
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
-                          Spacer(),
-                          GestureDetector(
-                              onTap: () {
-                                controller.removeCity(widget._city.uid);
-                              },
-                              child: Icon(
-                                Icons.delete_forever,
-                                color: Color(0xFF6c63ff),
-                              )),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 390,
-                          height: 250,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color.fromARGB(235, 221, 218, 218),
-                                offset: Offset(5, 2),
-                                blurRadius: 4.0,
-                                spreadRadius: 1.0,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 13, top: 30, right: 13),
+                            child: Row(
+                              children: [
+                                Text(
+                                  widget._city.cityName,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700,
+                                      color: Color(0xFF6c63ff)),
+                                ),
+                                Spacer(),
+                                GestureDetector(
+                                    onTap: () {
+                                      controller.removeCity(widget._city.uid);
+                                    },
+                                    child: Icon(
+                                      Icons.delete_forever,
+                                      color: Color(0xFF6c63ff),
+                                    )),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                width: 390,
+                                height: 250,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(widget._city.imageUrl),
+                                  ),
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Color.fromARGB(235, 221, 218, 218),
+                                      offset: Offset(5, 2),
+                                      blurRadius: 4.0,
+                                      spreadRadius: 1.0,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: 390,
+                                child: Text(
+                                  widget._city.content,
+                                  style: TextStyle(fontWeight: FontWeight.w500),
+                                ),
                               ),
                             ],
                           ),
-                          child:
-                              Image(image: NetworkImage(widget._city.imageUrl)),
-                        ),
-                        Column(
-                          children: [
-                            Container(
-                              width: 390,
-                              height: 250,
-                              child: SingleChildScrollView(
-                                child: Text(widget._city.content),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           );
